df8c63a265369da7eed9324fdbdd5ca53281127b
Vitaly Pavlenko <cxielamiko@gmail.com>
2020-04-09 15:13:00 +0300

    Class-based views

--- snippets/views.py
+++ snippets/views.py
@@ -1,57 +1,56 @@
 from typing import List, Optional
 
+from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-@api_view(["GET", "POST"])
-def snippet_list(request: Request, format: Optional[List[str]] = None) -> Response:
-    """
-    List all code snippets, or create a new snippet.
-    """
-    if request.method == "GET":
+class SnippetList(APIView):
+    def get(self, request: Request, format: Optional[List[str]] = None) -> Response:
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request: Request, format: Optional[List[str]] = None) -> Response:
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    raise AssertionError("not reachable")
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def snippet_detail(
-    request: Request, pk: int, format: Optional[List[str]] = None
-) -> Response:
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class SnippetDetail(APIView):
+    def get_object(self, pk: int) -> Snippet:
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
 
-    if request.method == "GET":
+    def get(
+        self, request: Request, pk: int, format: Optional[List[str]] = None
+    ) -> Response:
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == "PUT":
+    def put(
+        self, request: Request, pk: int, format: Optional[List[str]] = None
+    ) -> Response:
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == "DELETE":
+    def delete(
+        self, request: Request, pk: int, format: Optional[List[str]] = None
+    ) -> Response:
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=204)
-
-    raise AssertionError("not reachable")
